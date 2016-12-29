ae8cf60627cca642ffdb83655fa4b76ca887625f
Peter Hollands <peter.hollands@gmail.com>
2016-12-29 12:29:45 +0000

    options.h: High speed telemetry at 921600 baud which is 1 Mega baud when received at Raspberry Pi A+.

--- Config/options.h
+++ Config/options.h
@@ -368,8 +368,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Serial Output BAUD rate for either standard telemetry streams or MAVLink
 //  19200, 38400, 57600, 115200, 230400, 460800, 921600 // yes, it really will work at this rate
-#define SERIAL_BAUDRATE                     57600
-
+#define SERIAL_BAUDRATE		921600
 
 // NUM_ANALOG_INPUTS:
 // For UDB4 boards: Set to 0-4.  Analog pins are AN15 - AN18.
