a1a8a88e717b473661120f97dddd3ebee1157141
Peter Hollands <peter.hollands@gmail.com>
2018-08-10 14:04:48 +0100

    Move to stabilized mode with dead reckoning within 0.25 seconds after power up.

--- MatrixPilot/states.c
+++ MatrixPilot/states.c
@@ -37,7 +37,8 @@ static uint8_t counter = 0;
 #define CALIB_PAUSE   (2 * FSM_CLK)     // wait for 2 seconds of runs through the state machine
 #define STANDBY_PAUSE (5 * FSM_CLK)     // pause for 5 seconds of runs through the state machine
 #else
-#define CALIB_PAUSE (10.5 * FSM_CLK)    // wait for 10.5 seconds of runs through the state machine
+#define CALIB_PAUSE (0.25 * FSM_CLK)    // PDH: ChuckIt testing....assumes DCM_CALIB_PAUSE set to 0.125 seconds
+//#define CALIB_PAUSE (10.5 * FSM_CLK)    // wait for 10.5 seconds of runs through the state machine
 #define STANDBY_PAUSE (48 * FSM_CLK)    // pause for 48 seconds of runs through the state machine
                                         // This used to be 24 seconds, but that was not long enough
                                         // Standby pause was raised from 24 seconds to 48 seconds by BillP
