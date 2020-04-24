2cf46007f674c5f62e4944c3a7512c5b5e98920e
Peter Hollands <peter.hollands@gmail.com>
2020-04-24 21:12:36 +0100

    options.h: Terrain Follow on

--- Config/options.h
+++ Config/options.h
@@ -453,7 +453,7 @@
 #define USE_RANGER_INPUT                     7
 // set to RANGER_NONE, RANGER_LIDAR or RANGER_SONAR
 #define RANGER_TYPE                          RANGER_LIDAR
-#define RANGER_TERRAIN_FOLLOW                0
+#define RANGER_TERRAIN_FOLLOW                1
 ////////////////////////////////////////////////////////////////////////////////
 // Trigger Action
 // Use the trigger to do things like drop an item at a certain waypoint, or take a photo every
